6965730690b7323a91a8dc2aa7cd37845edb4702
carlasabata <2024_carla.sabata@iticbcn.cat>
2025-11-07 17:20:44 +0100

    x

---
+++ 2 Cookies/ex1 comptador_visites/comptador_visites.php
@@ -0,0 +1,5 @@
+<?php
+
+$des20="Oferta exclusiva! Utilitza el codi BOTIGA20 per obtenir un 20% de descompte en les teves primeres compres a la botiga"
+$des50="Oferta exclusiva sols per a tu! Utilitza el codi BOTIGA50 per obtenir un 50% de descompte en les teves primeres compres a la botiga"
+?>
