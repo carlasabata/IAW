559ea0dabccaf37e7207fbfef3d9612d7832dbd4
diacassandra <2024_carla.sabata@iticbcn.cat>
2025-11-11 10:29:07 +0100

    Update bodega.php

---
+++ 2 Cookies/ex 2 bodega/bodega.php
@@ -0,0 +1,22 @@
+<?php
+$edat=$_GET['maj_edat'];
+$idioma=$_GET['llengua'];
+$moneda=$_GET['mon'];
+
+# if (edat>18) no et poden vendre alcohol
+
+# if (idioma=cat & moneda=eur) ----- t'oferim el vi "Les Terrasses" a un preu de 39€.
+
+
+# la botiga consta de només 2 productes
+# fer traducció per als diferents idiomes i monedes
+
+
+
+# estructura condicional
+if ($moneda == 'euros') {
+  echo ;
+} else {
+  echo "";
+}
+?>
