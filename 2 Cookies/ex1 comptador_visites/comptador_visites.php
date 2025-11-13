a6e678d440211efa4c799e376df422590b6cd90e
carlasabata <2024_carla.sabata@iticbcn.cat>
2025-11-13 18:40:03 +0100

    m

--- 2 Cookies/ex1 comptador_visites/comptador_visites.php
+++ 2 Cookies/ex1 comptador_visites/comptador_visites.php
@@ -1,5 +1,6 @@
 <?php
-
-$des20="Oferta exclusiva! Utilitza el codi BOTIGA20 per obtenir un 20% de descompte en les teves primeres compres a la botiga"
-$des50="Oferta exclusiva sols per a tu! Utilitza el codi BOTIGA50 per obtenir un 50% de descompte en les teves primeres compres a la botiga"
+# if comptadors visites = 5 desc=20%
+# "                     " 10 desc = 50 + no ha fet servir el desc previament
+# $des20="Oferta exclusiva! Utilitza el codi BOTIGA20 per obtenir un 20% de descompte en les teves primeres compres a la botiga"
+# $des50="Oferta exclusiva sols per a tu! Utilitza el codi BOTIGA50 per obtenir un 50% de descompte en les teves primeres compres a la botiga"
 ?>
--- Exercicis sintaxi inicial/ex2 conversio_anys/conversio_anys.php
+++ Exercicis sintaxi inicial/ex2 conversio_anys/conversio_anys.php
@@ -1,4 +1,5 @@
 <?php
 $numero=$_GET['num'];
+# 30 dies 1 mes
 
 ?>
